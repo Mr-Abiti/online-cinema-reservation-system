44dc70066e904c671b7dbff9b186125152f90597
muluken bogale <mulukenb89@gmail.com>
2018-12-22 00:03:28 +0300

    finish up get and post for movies routes

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const movie =require('../models/movies');
+const movie = require('../models/movies');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
@@ -10,37 +10,49 @@ router.use(bodyParser.urlencoded({
 }));
 
 
-router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message:'movie route'
-     });
+router.get('/', (req, res, next) => {
+    movie.find()
+        .exec()
+        .then(doc => {
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+
+            })
+        })
 });
 
-router.post('/',(req,res,next)=>{
+router.post('/', (req, res, next) => {
     const movies = new movie({
         _id: new mongoose.Types.ObjectId,
         name: req.body.name,
     });
     movies.save()
-        .then(result => console.log(result))
-        .catch(err => console.log(err))
-    res.status(201).json({
-        message: 'movie is created',
-        movies: movies
-    });
+    .then(result =>{
+        console.log(result);
+        res.status(201).json(result);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error:err
+        });
+    })
 
 
-});
-
-router.put('/',(req,res,next)=>{
-
-});
-
-router.delete('/:id',(req,res,next)=>{
 
 });
 
-router.patch('/:id',(req,res,next)=>{
+router.put('/', (req, res, next) => {
+
+});
+
+router.delete('/:id', (req, res, next) => {
+
+});
+
+router.patch('/:id', (req, res, next) => {
 
 });
 
