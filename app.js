4d427666a83962fd51a8d6597326bfd72528c2a9
muluken bogale <mulukenb89@gmail.com>
2018-12-22 00:00:06 +0300

    add error handling method for developmen

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const movieRoute = require('./routes/movies');
 const cinemaRoute = require('./routes/cinemas');
 
 const app = express();
-
-app.use('/user',userRoute);
-app.use('/movie',movieRoute);
-app.use('/cinema',cinemaRoute);
+app.use(morgan('dev'));
 
 mongoose.connect('mongodb://localhost/OCR')
     .then(() => console.log('connected to mongodb.... '))
@@ -20,6 +17,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(morgan('dev'));
+
+app.use('/user', userRoute);
+app.use('/movie', movieRoute);
+app.use('/cinema', cinemaRoute);
+
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status=404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+});
 
 module.exports = app;
